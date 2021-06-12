8298cdb049fe6f786a550f28dcb9b3d22f8b15d1
jakarahman18 <jakarahman18@gmail.com>
2021-06-12 21:19:48 +0700

    format codes with eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello Guys!</h1>
+    <div className="App">
+      <section className="bg-green-50 h-screen w-screen">
+        <div className="p-10 rounded-xl bg-white w-60">
+          <h2>Test</h2>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
