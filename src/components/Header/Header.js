8906a2ba0773ef56250352bcba7ad2123c518d4a
karahman34 <jakarahman007@gmail.com>
2021-06-15 08:55:00 +0700

    add feature to change languange

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,36 @@
+import { useState, useCallback } from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { setLang } from 'store/modules/global/actions'
+import { useTranslation } from 'react-i18next'
+import PropTypes from 'prop-types'
 
-function Header() {
+const mapStateToProps = ({ global }) => ({
+  lang: global.lang,
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  setLang: (lang) => dispatch(setLang(lang)),
+})
+
+function Header({ lang, setLang }) {
+  const { i18n } = useTranslation()
+
+  const [selectedLang, setSelectedLang] = useState(lang)
+
+  const langChangeHandler = useCallback((e) => {
+    const newLang = e.target.value
+
+    // Change lang state.
+    setLang(newLang)
+
+    // Change lang local input.
+    setSelectedLang(newLang)
+
+    // Change lang in i18n.
+    i18n.changeLanguage(newLang)
+  })
+
   return (
     <div className="py-3 mb-5 flex justify-between">
       {/* Logo */}
@@ -9,7 +39,11 @@ function Header() {
       </Link>
 
       {/* Lang */}
-      <select className="bg-white py-2 px-3 rounded-md focus:outline-none focus:ring focus:ring-green-500 transition-all">
+      <select
+        className="bg-white py-2 px-3 rounded-md focus:outline-none focus:ring focus:ring-green-500 hover:cursor-pointer transition-all"
+        onChange={langChangeHandler}
+        value={selectedLang}
+      >
         <option value="id">Indonesia</option>
         <option value="en">English</option>
       </select>
@@ -17,4 +51,9 @@ function Header() {
   )
 }
 
-export default Header
+Header.propTypes = {
+  lang: PropTypes.string.isRequired,
+  setLang: PropTypes.func.isRequired,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
